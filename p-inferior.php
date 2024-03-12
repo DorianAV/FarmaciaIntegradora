c4a107a2b7b484034ea159b696e20cf1e39479e2
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-03-12 13:24:16 -0600

    v1.1

--- p-inferior.php
+++
@@ -1,9 +0,0 @@
-<footer class="fixed-bottom">
-    <ul class="nav justify-content-center navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
-        <li class="nav-item">
-            <a class="nav-link disabled" aria-current="page" >Derechos reservados</a>
-        </li>
-    </ul>
-</footer>
-</body>
-</html>
