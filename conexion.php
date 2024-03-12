3a3ff9f4203565d8dfff4ada9a2e976f5597a6dd
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-03-12 13:08:51 -0600

    v1.1

--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php
 
-$con=mysqli_connect('localhost','root','','restaurant' );
+$con=mysqli_connect('localhost','root','','farcaciaintegradora' );
 if(!$con){
     echo"Error en la conexion de la base de datos";
     exit;
 }
+echo "Hola imanoil";
 
 ?>
